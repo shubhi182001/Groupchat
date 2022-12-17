ebe36ab4f50019a5f4b51635f703cd33f67b4c40
shubhi182001 <kediashubhi@gmail.com>
2022-12-17 20:05:14 +0530

    dynamic messaging and scroll down feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
+import { useEffect } from 'react';
 import './App.css';
 import Chat from './components/chats/Chat';
 import Sidebar from './components/sidebar/Sidebar';
+import Pusher from "pusher-js"
+import axios from './axios';
+import { useState } from 'react';
 
 
-function App() {
+
+const App = () => {
+
+  const[messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('/messages/sync')
+    .then(res => {
+      console.log(res.data);
+      setMessages(res.data); 
+    });
+  },[])
+
+
+  useEffect(() => {
+    const pusher = new Pusher('ed94a8b9ce8932a13626', {
+      cluster: 'ap2'
+    });
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage]);
+    });
+
+
+    return() => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+
+  },[messages])
+
+  console.log(messages)
+
+
   return (
     <div className="app">
       <div className="app-body">
         <Sidebar/>  
-        <Chat/>
+        <Chat messages = {messages} />
       </div>
      </div> 
   );
--- src/components/chats/Chat.jsx
+++ src/components/chats/Chat.jsx
@@ -6,7 +6,27 @@ import AttachFileOutlinedIcon from '@mui/icons-material/AttachFileOutlined';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 import MicNoneOutlinedIcon from '@mui/icons-material/MicNoneOutlined';
-const Chat = () => {
+import { useState, useRef, useEffect } from 'react';
+import axios from '../../axios';
+
+const Chat = ({messages}) => {
+  const[inputMessage, setInputMessage] = useState("");
+
+  const bottomRef = useRef(null);
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({behavior: 'smooth'});
+  }, [messages]);
+
+  const postingMessage = async(e) => {
+    e.preventDefault();
+    await axios.post("messages/new", {
+      message: inputMessage,
+      name:"shubhi",
+      timestamp: "Just now",
+      recieved: false
+    })
+    setInputMessage("");
+  }
   return (
     <div className='chat'>
        <div className="chat-header">
@@ -28,39 +48,26 @@ const Chat = () => {
         </div>
        </div>
        <div className="chat-body">
-        <p className='chat-message'>
+        {messages?
+        messages.map((m) => (
+          <p className={m.recieved?'chat-message': 'chat-message chat-reciever'}>
           <span className="chat-name">
-            Sonny
+            {m.name}
           </span>
-          This is a message
+          {m.message}
           <span className="chat-timestamp">
-            {new Date().toUTCString()}
+            {m.timestamp}
           </span>
         </p>
-        <p className=' chat-message chat-reciever'>
-          <span className="chat-name">
-            Sonny
-          </span>
-          This is a message
-          <span className="chat-timestamp">
-            {new Date().toUTCString()}
-          </span>
-        </p>
-        <p className='chat-message'>
-          <span className="chat-name">
-            Sonny
-          </span>
-          This is a message
-          <span className="chat-timestamp">
-            {new Date().toUTCString()}
-          </span>
-        </p>
+        )
+        ):""}
+       <div ref={bottomRef} />
        </div>
        <div className="chat-footer">
           <InsertEmoticonIcon/>
            <form>
-            <input type="text" placeholder='Type a message' />
-            <button type='submit'>Send a message</button>
+            <input type="text" placeholder='Type a message' value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} />
+            <button type='submit' onClick={postingMessage}>Send a message</button>
            </form>
           <MicNoneOutlinedIcon/>
        </div>
